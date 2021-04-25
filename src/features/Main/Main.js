788b37d1394994e07dfe5e13ed46a12068a03578
nesherson <nesad.omeragich27@gmail.com>
2021-04-25 02:54:02 +0200

    Added styled-components

---
+++ src/features/Main/Main.js
@@ -0,0 +1,12 @@
+import Styled from 'styled-components';
+
+const MainPage = Styled.main`
+    background-color: #f4f4f4;
+    color: #333;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+`;
+
+
+const Main = () => {
+    return null;
+}
