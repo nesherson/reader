a4c410a8ceaf80bd7fc5ade6c16b4f49e0ccc395
nesherson <nesad.omeragich27@gmail.com>
2021-04-25 19:37:08 +0200

    Added PostList and Post components

---
+++ src/features/Main/postList/post/Post.js
@@ -0,0 +1,16 @@
+import Styled from 'styled-components';
+
+const PostItem = Styled.li`
+    padding: 10px 10px 10px 17px;
+    margin-bottom: 7px;
+    @hover: background-color: #EEE;
+`;
+
+const PostTitle = Syled.h3`
+    margin-top: 0;
+    margin-bottom: 5px;
+`;
+
+export const Post = () => {
+
+}
---
+++ src/features/Main/postList/postList.js
@@ -0,0 +1,11 @@
+import Styled from 'styled-components';
+
+const List = Styled.ul`
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    `;
+
+export const PostList = () => {
+    return null;
+}
