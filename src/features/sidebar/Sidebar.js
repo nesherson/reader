3dd591e1083af768708c6b0b7e1c4b701cc338bc
nesherson <nesad.omeragich27@gmail.com>
2021-04-25 03:10:06 +0200

    Added Sidebar component

---
+++ src/features/sidebar/Sidebar.js
@@ -0,0 +1,15 @@
+import Styled from 'styled-components';
+
+const Aside = Styled.aside`
+    flex: 1;
+    min-width: 220px;
+`;
+
+
+export const Sidebar = () => {
+    return (
+        <Aside>
+            Sidebar
+        </Aside>
+    );
+}
