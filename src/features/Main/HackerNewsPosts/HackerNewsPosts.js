464ea458994b573d057a810150b39a2a05be3878
nesherson <nesad.omeragich27@gmail.com>
2021-04-26 02:47:24 +0200

    Added hacker news posts and redux state

---
+++ src/features/Main/HackerNewsPosts/HackerNewsPosts.js
@@ -0,0 +1,29 @@
+import Styled from 'styled-components';
+import { PostList } from './postList/PostList';
+import { selectHackerNewsPosts, fetchHackerNewsPosts } from './HackerNewsPostsSlice.js';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+
+const Header = Styled.h1`
+    margin-left: 25px;
+`;
+
+export const HackerNewsPosts = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchHackerNewsPosts());
+    }, [dispatch]);
+    
+    const posts = useSelector(selectHackerNewsPosts);
+
+    console.log('hackerNews --> ', posts);
+
+    return (
+        <div>
+            <Header>Hacker News</Header>
+            { !posts ? <h2>Loading...</h2> : <PostList posts={posts}/> }
+        </div>
+    );
+}
